398551068fa2eb24d18439ff2aa1363014292c9a
rawatsaheb5 <diamondcut2004@gmail.com>
2024-11-16 15:44:48 +0530

    added user model

---
+++ prisma/migrations/20241116101042_user_schema/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT,
+    "email" TEXT NOT NULL,
+    "password" TEXT,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
