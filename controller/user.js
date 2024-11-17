f0e93ce996f17371970a17c7e5b79130fd5f9897
rawatsaheb5 <diamondcut2004@gmail.com>
2024-11-17 15:40:20 +0530

    added login route

--- controller/user.js
+++ controller/user.js
@@ -27,3 +27,22 @@ export const register = async (req, res) => {
         return res.status(500).json({message:"internal server error"})
     }
 }
+export const login = async (req, res) => {
+    try {
+        const {  email, password } = req.body;
+        //console.log(req.body)
+        const user = await prisma.user.findUnique({
+            where: {
+                email: email,
+                password:password
+            }
+        })
+        if (user === null) {
+            return res.status(400).json({message:"invalid credentials"})
+        }  
+        res.status(200).json({message:"user login successfully!"})
+    } catch (error) {
+        console.log(error)
+        return res.status(500).json({message:"internal server error"})
+    }
+}
--- route/userRoute.js
+++ route/userRoute.js
@@ -1,8 +1,8 @@
 import express from 'express'
-import { register } from '../controller/user.js';
+import { login, register } from '../controller/user.js';
 const router = express.Router();
 
 
 router.post('/signup', register)
-
+router.post('/signin', login)
 export default router;
